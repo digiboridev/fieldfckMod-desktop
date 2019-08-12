e0ec47a05901ffd97b8b32ee9fc0d25ea7224a62
Im a desktop <digiborimusik@gmail.com>
2019-08-12 12:55:30 +0300

    add some

---
+++ main.js
@@ -0,0 +1,89 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+// var client = require('node-rest-client-promise').Client();
+
+var Model = require('./model').Model;
+var Profile = require('./model').Profile;
+
+function l(a){
+    console.log(a)
+}
+l('main run')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+  console.log('window run');
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+const model = new Model();
+
+model.additem(
+    new Profile({
+        login:'vkomelkov',
+        password:"Qwer1111",
+        intervals:{
+            aTob:20,
+            bToc:30
+        },
+        gpsPattern:[
+            {long:42.123123,lat:34.321321},
+            {long:44.123123,lat:36.321321},
+            {long:45.123123,lat:31.321321},
+            {long:46.123123,lat:37.321321}
+        ]})
+)
+model.additem(
+    new Profile({
+        login:'fghh',
+        password:"123"
+    })
+)
+
+console.log(model.getarr());
+model.loadActivityData('vkomelkov').then(a => {
+    l(model.getitem('vkomelkov').showdata());
+}).catch(a => l("error " + a));
---
+++ model.js
@@ -0,0 +1,123 @@
+var client = require('node-rest-client-promise').Client();
+
+
+function l(a){
+    console.log("\nModel:")
+    console.log(a)
+    console.log("\n")
+}
+l('model run')
+
+
+const globalSettings = {
+    url:"https://ffm.ukrtelecom.net",
+    intervals:{
+        aTob:25,
+        bToc:30
+    },
+    gpsPattern:[
+        {long:42.123123,lat:34.321321},
+        {long:44.123123,lat:36.321321},
+        {long:45.123123,lat:31.321321},
+        {long:46.123123,lat:37.321321}
+    ]
+}
+
+
+class Model{
+    constructor(){
+        this.arr = [];
+    }
+    additem(data){
+        this.arr.push(data)
+    }
+    getarr(){
+        return this.arr;
+    }
+    getitem(key){
+       return this.arr.find(item => item.key === key);
+    }
+    updateitem(key,data){
+        const item = this.getitem(key);
+        l(item)
+        for(let o in item){
+            data[o] !== undefined ?  item[o] = data[o] : undefined;
+        }
+        l(item)
+    }
+    removeitem(key){
+        const index = this.arr.findIndex(item => item.key == key);
+        this.arr.splice(index, 1)
+    }
+    loadActivityData(key){
+        const profile = this.arr.find(item => item.key === key);
+
+        return new Promise(function(resolve,reject){
+            client.postPromise(
+                globalSettings.url + "/ServiceModel/AuthService.svc/Login",
+                {
+                    data: JSON.stringify({UserName:profile.login,UserPassword:profile.password}),
+                    headers: { "Content-Type": "application/json" }
+                }
+            )
+            .then(a => {
+                // resolve(a.data)
+                if(a.data.Code !== 0){
+                    reject(a.data.Message)
+                    throw '0'
+                }
+                // profile.data = a.data;
+                let cookie = a.response.headers['set-cookie'];
+                l(cookie);
+                return a;
+                
+            })
+            .then(a => {
+                let cookie = a.response.headers['set-cookie'];
+                let csrftoken =  cookie[3].slice(8,-8);
+                l(cookie);
+                l(csrftoken);
+                let getargs = {
+                    headers: { "Content-Type": "application/json;odata=verbose" , "Accept": "application/json;odata=verbose" , "Cookie": cookie ,  "BPMCSRF": csrftoken }
+                }
+                return client.getPromise(globalSettings.url + "/0/ServiceModel/EntityDataService.svc/ActivityCollection(guid'b479aa31-4f95-4c85-bba9-3636b4e8e689')",getargs)
+            })
+            .then(a => {
+                l(JSON.parse(a.data));
+                resolve ('Data downloaded');
+            })
+            .catch(a => reject('Network error'));            
+
+
+        })
+    }
+}
+
+const model = new Model();
+
+class Profile{
+    constructor(data){
+        this.key = data.login;
+        this.login = data.login;
+        this.password = data.password;
+        this.intervals = (data.intervals !== undefined ? data.intervals : globalSettings.intervals);
+        this.gpsPattern = (data.gpsPattern !== undefined ? data.gpsPattern : globalSettings.gpsPattern);
+        this.data = {};
+    }
+    showdata(){
+        return this.data
+    }
+
+}
+
+
+// model.loadActivityData('vkomelkov').then(a => {
+//     l(model.getitem('vkomelkov').showdata());
+// }).catch(a => l("error " + a));
+
+
+
+module.exports = {
+    Model,
+    Profile
+};
